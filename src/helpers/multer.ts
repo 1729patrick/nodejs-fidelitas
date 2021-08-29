9a44ae3f1debed0c5e4a40302ab200c328d201fb
Patrick Battisti Forsthofer <patrick.forsthofer@lisgo.me>
2021-08-29 20:38:21 +0100

    removed local files

--- src/helpers/multer.ts
+++ src/helpers/multer.ts
@@ -1,6 +1,6 @@
 import multer from "multer";
 import { resolve } from "path";
 
-const upload = multer({ dest: resolve(__dirname, "..", "..", "files") });
+const upload = multer({ dest: resolve(__dirname, "..", "..", "tmp", "files") });
 
 export default () => upload.single("file");
