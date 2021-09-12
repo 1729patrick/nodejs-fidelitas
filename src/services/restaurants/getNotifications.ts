afd84fd0e16dc7f8c98e5e0ff7e9627ecceebf24
Tomás <tommasini15@gmail.com>
2021-09-12 19:52:25 +0100

    getNotifications

--- src/services/restaurants/getNotifications.ts
+++ src/services/restaurants/getNotifications.ts
@@ -1,16 +1,14 @@
 import { Request, Response } from "express";
-import { GetRestaurantParams } from "../../../types/requests/restaurant/getRestaurant";
 
 import getNotifications from "../../data/restaurants/getNotifications";
 
 export default async (
-  req: Request<any, GetRestaurantParams>,
+  req: Request,
   res: Response
 ) => {
   try {
     const { restaurantId } = req.params;
-
-    const notifications = await getNotifications(restaurantId);
+    const notifications = await getNotifications(parseInt(String(restaurantId)));
     return res.json(notifications);
   } catch (err: any) {
     return res.boom.badRequest(err.message);
