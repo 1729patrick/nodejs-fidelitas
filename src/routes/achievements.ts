d4d7b7808ccf71e20f14e5c662b84cf001803b87
Patrick Battisti Forsthofer <patrick.forsthofer@lisgo.me>
2021-09-13 22:41:45 +0100

    fix achievements

--- src/routes/achievements.ts
+++ src/routes/achievements.ts
@@ -1,22 +1,23 @@
-import { Router } from "express";
-import getAchievements from "../services/achievements/getAchievements";
-import addAchievements from "../data/achievements/addAchievements";
-import {celebrate, Joi, Segments} from "celebrate";
+import { Router } from 'express';
+import getAchievements from '../services/achievements/getAchievements';
+import { celebrate, Joi, Segments } from 'celebrate';
+import addAchievements from '../services/achievements/addAchievements';
 
 const router = Router();
 
-router.route("/").get(getAchievements);
-router.route("/add").put( celebrate({
-  [Segments.BODY]: Joi.object().keys({
-    title: Joi.string().required(),
-    description: Joi.string().required(),
-    type: Joi.string().required(),
-    reward: Joi.string().required(),
-    rewardValue: Joi.number().required(),
-    cost: Joi.number().required(),
+router.route('/').get(getAchievements);
+router.route('/add').put(
+  celebrate({
+    [Segments.BODY]: Joi.object().keys({
+      title: Joi.string().required(),
+      description: Joi.string().required(),
+      type: Joi.string().required(),
+      reward: Joi.string().required(),
+      rewardValue: Joi.number().required(),
+      cost: Joi.number().required(),
+    }),
   }),
-}),
-  // @ts-ignore
-  addAchievements,);
+  addAchievements,
+);
 
 export default router;
