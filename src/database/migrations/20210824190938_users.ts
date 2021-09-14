bf05c23eb3d255401c01a163d9bcec477e3cf467
Tomás <tommasini15@gmail.com>
2021-09-14 19:17:08 +0100

    Migrations updatae with column status

--- src/database/migrations/20210824190938_users.ts
+++ src/database/migrations/20210824190938_users.ts
@@ -12,6 +12,7 @@ export async function up(knex: Knex): Promise<void> {
       t.enum("type", ["client", "admin"]).notNullable();
       t.string("phone").unique().index().notNullable();
       t.string("email").unique().index().notNullable();
+      t.enum("status", ["ACTIVE", "DELETED"]).notNullable();
       t.integer("restaurantId").notNullable().index();
       t.timestamp("createdAt").defaultTo(knex.fn.now());
       t.timestamp("updatedAt").defaultTo(knex.fn.now());
--- src/database/migrations/20210827210303_products.ts
+++ src/database/migrations/20210827210303_products.ts
@@ -20,6 +20,7 @@ export async function up(knex: Knex): Promise<void> {
         "special",
         "salad",
       ]);
+      t.enum("status", ["ACTIVE", "DELETED"]).notNullable();
       t.integer("imageId").notNullable();
       t.integer("restaurantId").notNullable().index();
       t.timestamp("createdAt").defaultTo(knex.fn.now());
