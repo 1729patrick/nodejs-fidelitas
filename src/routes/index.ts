4baabfb9c354a01448d0c85f8bfa8637a8420e7c
Patrick Battisti Forsthofer <patrick.forsthofer@lisgo.me>
2021-09-18 22:42:28 +0100

    fidelitas api

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,40 +1,40 @@
-import * as winston from "winston";
-import { Express, Request, Response } from "express";
-import { errors } from "celebrate";
+import * as winston from 'winston';
+import { Express, Request, Response } from 'express';
+import { errors } from 'celebrate';
 
-import authMiddleware from "../middlewares/auth";
+import authMiddleware from '../middlewares/auth';
 
-import auth from "./auth";
-import restaurants from "./restaurants";
-import products from "./products";
-import files from "./files";
-import achievements from "./achievements";
-import addresses from "./addresses";
-import user from "./user";
-import purchases from "./purchases";
-import facilities from "./facilities";
+import auth from './auth';
+import restaurants from './restaurants';
+import products from './products';
+import files from './files';
+import achievements from './achievements';
+import addresses from './addresses';
+import user from './user';
+import purchases from './purchases';
+import facilities from './facilities';
 
 export default (app: Express) => {
-  winston.log("info", "--> Initialisations des routes");
+  winston.log('info', '--> Initialisations des routes');
 
-  app.get("/api", (_: Request, res: Response) =>
+  app.get('/fidelitas/api', (_: Request, res: Response) =>
     res.status(200).send({
-      message: "server is running",
-    })
+      message: 'server is running',
+    }),
   );
 
-  app.use("/auth", [auth]);
-  app.use("/files", [files]);
-  app.use("/restaurants", [restaurants]);
+  app.use('/auth', [auth]);
+  app.use('/files', [files]);
+  app.use('/restaurants', [restaurants]);
 
   app.use(authMiddleware);
-  app.use("/products", [products]);
-  app.use("/achievements", [achievements]);
-  app.use("/addresses", [addresses]);
-  app.use("/user", [user]);
-  app.use("/purchases", [purchases]);
-  app.use("/facilities", [facilities]);
+  app.use('/products', [products]);
+  app.use('/achievements', [achievements]);
+  app.use('/addresses', [addresses]);
+  app.use('/user', [user]);
+  app.use('/purchases', [purchases]);
+  app.use('/facilities', [facilities]);
 
   app.use(errors());
-  app.all("*", (_: Request, res: Response) => res.boom.notFound());
+  app.all('*', (_: Request, res: Response) => res.boom.notFound());
 };
